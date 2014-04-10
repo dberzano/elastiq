bcd293152d3a7f4b0bbc5d5f15fb1ddd17786bd4
Dario Berzano <dario.berzano@cern.ch>
2014-04-10 17:28:40 +0200

    Jobs threshold default set to zero

--- elastiq/__init__.py
+++ elastiq/__init__.py
@@ -32,7 +32,7 @@ cf['elastiq'] = {
   'estimated_vm_deploy_time_s': 600,
 
   # Conditions to start new VMs
-  'waiting_jobs_threshold': 10,
+  'waiting_jobs_threshold': 0,
   'waiting_jobs_time_s': 100,
   'n_jobs_per_vm': 4,
 
