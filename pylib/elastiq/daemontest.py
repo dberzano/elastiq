154a4fc85a06dc2d5da985f0e6d3da031c5dd286
Dario Berzano <dario.berzano@cern.ch>
2015-04-15 12:59:32 +0900

    Elastiq: support logfiles and rotate them

--- pylib/elastiq/daemontest.py
+++ pylib/elastiq/daemontest.py
@@ -1,5 +1,7 @@
 from daemon import Daemon
 import time
+import logging, logging.handlers
+import os
 
 class Elastiq(Daemon):
 
@@ -15,12 +17,37 @@ class Elastiq(Daemon):
     self._conffile = conffile
     self._logdir = logdir
     self._statefile = statefile
+    self._setup_log_files()
+
+
+  ## Setup use of logfiles, rotated and deleted periodically.
+  def _setup_log_files(self):
+
+    if not os.path.isdir(self._logdir):
+      os.mkdir(self._logdir, 0700)
+    else:
+      os.chmod(self._logdir, 0700)
+
+    format = '%(asctime)s %(name)s %(levelname)s [%(module)s.%(funcName)s] %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
+
+    log_file_handler = logging.handlers.RotatingFileHandler(self._logdir+'/elastiq.log',
+      mode='a', maxBytes=1000000, backupCount=30)
+
+    log_file_handler.setFormatter(logging.Formatter(format, datefmt))
+    log_file_handler.doRollover()
+
+    self.logctl.addHandler(log_file_handler)
+
 
   ## Action to perform when some exit signal is received.
   #
   #  @return When returning True, exiting continues, when returning False exiting is cancelled
   def onexit(self):
+    self.logctl.info('Acklowledging exit request')
+    time.sleep(1)
     return True
+
 
   ## Main loop
   #
@@ -28,7 +55,10 @@ class Elastiq(Daemon):
   def run(self):
 
     while True:
-      print 'ciao'
+      self.logctl.debug('Hello world (debug)')
+      self.logctl.info('Hello world (info)')
+      self.logctl.warning('Hello world (warning)')
+      self.logctl.error('Hello world (error)')
       time.sleep(1)
 
     return 0
