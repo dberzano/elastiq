cab121e0c2c757d49659f0826741c079d00e7cbe
Dario Berzano <dario.berzano@cern.ch>
2015-05-31 18:39:36 +0200

    Docker: fake Condor cmdd, EC2 envvars, custom conf

--- misc/docker-entrypoint.sh
+++ misc/docker-entrypoint.sh
@@ -17,6 +17,44 @@ else
   exit 2
 fi
 
+sconf="$(dirname "$0")/elastiq.conf"
+econf='/etc/elastiq.conf'
+if [[ -e "$sconf" ]] ; then
+  echo '--> Applying new configuration'
+  cat "$sconf" > "$econf"
+fi
+
+echo '--> Exporting EC2 variables'
+function get_var() (
+  cat "$econf" | grep "$1" | sed -e "s|^\s*$1\s*=\s*||"
+)
+export EC2_URL=$(get_var api_url)
+export EC2_ACCESS_KEY=$(get_var aws_access_key_id)
+export EC2_SECRET_KEY=$(get_var aws_secret_access_key)
+
+echo '--> Creating fake condor stuff'
+cat > /usr/bin/condor_q <<EOF
+#!/bin/bash
+echo JobStatus = 1
+EOF
+cat > /usr/bin/condor_status <<EOF
+#!/bin/bash
+cat <<_EoF_
+<?xml version="1.0"?>
+<!DOCTYPE classads SYSTEM "classads.dtd">
+<classads>
+</classads>
+_EoF_
+EOF
+chmod +x /usr/bin/condor_{q,status}
+
+echo '--> Setting elastiq state'
+st=/var/lib/elastiq/state
+cat > "$st" <<_EoF_
+i-00149e77
+_EoF_
+chown elastiq:elastiq "$st"
+
 echo '--> Running elastiq'
 service elastiq start
 
